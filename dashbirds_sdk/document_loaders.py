fecdf5a6631b50b3be33c05c9869c53174b2fdf8
edubragabonfim <ebbonfim@sga.pucminas.br>
2025-11-05 20:03:08 -0300

    remove_unecessary_tags + save_md_file

--- dashbirds_sdk/document_loaders.py
+++ dashbirds_sdk/document_loaders.py
@@ -1,11 +1,17 @@
+import os
 import sys
 from urllib.parse import urlparse
 
 from docling.document_converter import DocumentConverter
 from docling_core.types.doc.document import DoclingDocument
+from colorama import Fore, Style, init as init_colorama
 
 
-def is_url_valid(text: str) -> bool:
+# Inits
+init_colorama()
+
+
+def is_url_valid(text: str) -> bool:  # True or False
     '''Recebe um valor do tipo texto e retorna True se o texto for uma URL.'''
     if not isinstance(text, str):
         return False
@@ -19,12 +25,9 @@ def is_url_valid(text: str) -> bool:
 
 def convert_document(source: str) -> DoclingDocument:
     '''Recebe uma URL e converte em um objeto da classe DoclingDocument que pode ser processado.'''
-    if is_url_valid(source):
-        converter = DocumentConverter()
-        doc = converter.convert(source).document
-        return doc
-    else:
-        return DoclingDocument(name='')
+    converter = DocumentConverter()
+    doc = converter.convert(source).document
+    return doc
 
 
 def extract_file_name(document: DoclingDocument) -> str:
@@ -32,7 +35,15 @@ def extract_file_name(document: DoclingDocument) -> str:
     return file_name
 
 
+def remove_unnecessary_tags(content: str) -> str:
+    return content.replace('<!-- image -->', '')
+
+
 def extract_document(source: str) -> dict:
+
+    if not is_url_valid(source):
+        return {}
+
     doc = convert_document(source)
     
     # Extract File Name
@@ -41,13 +52,34 @@ def extract_document(source: str) -> dict:
     # Extract Content
     content = doc.export_to_markdown()
 
+    # Remove Tags
+    content = remove_unnecessary_tags(content)
+
     result = {'file_name': file_name, 'content': content}
 
     return result
 
 
-if __name__ == '__main__':
-    _source = sys.argv[1]
-    data = extract_document(_source)
+def save_md_file(content: str, file_name: str , docs_path: str = 'C:\\Dashbirds_SDK_outputs\\md') -> bool:
+    """Quando o usuário permite, é criada uma pasta com os outputs para uma pasta na raiz. (C://)"""
+    
+    # Cria o diretório se não existe.
+    if not os.path.isdir(docs_path):
+        os.makedirs(docs_path)
 
-    print(data['content'])
+    with open(f"{docs_path}/{file_name}.md", "w", encoding="utf-8") as file:
+        file.write(content)
+
+    return True
+
+
+if __name__ == '__main__':
+    source = sys.argv[1]
+    result = extract_document(source)
+
+    print(Fore.BLUE + result['content'] + Style.RESET_ALL)
+
+    if input('Save File? ').lower() == 's':
+        save_md_file(result['content'], result['file_name'])
+    
+    print('Done!')
